7ede0b0d84b9d56fb2a8de99b579093bfa47dbeb
Nicolás Céspede <cespede.alannicolas.42984@gmail.com>
2022-04-12 23:02:44 -0300

    Agregada respuesta diferida en -estadisticas

--- commands/1. general/estadisticas.js
+++ commands/1. general/estadisticas.js
@@ -46,7 +46,8 @@ module.exports = {
     guildOnly: true,
 
     callback: async ({ guild, message, client, interaction, instance, user }) => {
-        var messageOrInteraction = message ? message : interaction;
+        if (message) var deferringMessage = await message.reply({ content: 'Obteniendo estadísticas, por favor aguardá unos segundos...' });
+        else if (interaction) await interaction.deferReply({ ephemeral: true });
         var timestamps = getTimestamps();
         for (const key in timestamps)
             if (Object.hasOwnProperty.call(timestamps, key)) {
@@ -63,15 +64,14 @@ module.exports = {
             }).catch(() => console.log(`> El usuario con ID ${actualStat['stats_id']} ya no está en el servidor.`));
             timeField.value += `${timeToString(actualStat['stats_seconds'], actualStat['stats_minutes'], actualStat['stats_hours'], actualStat['stats_days'])}\n\n`;
         }
-        messageOrInteraction.reply({
-            embeds: [new MessageEmbed()
-                .setTitle(`**Estadísticas**`)
-                .setDescription(`Hola <@${user.id}>, el tiempo de conexión en chats de voz de los usuarios es:\n\n${usersField.value.length === 0 ? '_No hay estadísticas actualmente._' : ''}`)
-                .addFields(usersField.value.length != 0 ? [usersField, timeField] : [])
-                .setColor(instance.color)
-                .setThumbnail(client.user.avatarURL())],
-            ephemeral: true
-        });
+        const embed = new MessageEmbed()
+            .setTitle(`**Estadísticas**`)
+            .setDescription(`Hola <@${user.id}>, el tiempo de conexión en chats de voz de los usuarios es:\n\n${usersField.value.length === 0 ? '_No hay estadísticas actualmente._' : ''}`)
+            .addFields(usersField.value.length != 0 ? [usersField, timeField] : [])
+            .setColor(instance.color)
+            .setThumbnail(client.user.avatarURL());
+        if (message) deferringMessage.edit({ content: ``, embeds: [embed] });
+        else if (interaction) interaction.editReply({ embeds: [embed] });
         return;
     }
 }
