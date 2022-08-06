15fc69b3807f5545eef4c0ff9001a756b5efd719
Nicolás Céspede <cespede.alannicolas.42984@gmail.com>
2022-08-06 01:27:58 -0300

    Update borrar.js

--- commands/8. private/borrar.js
+++ commands/8. private/borrar.js
@@ -2,7 +2,7 @@ const { ids } = require("../../app/constants");
 
 module.exports = {
     category: 'Privados',
-    description: 'Elimina todos los mensajes del canal de mensajes directos.',
+    description: 'Elimina todos los mensajes del canal de mensajes directos de quien se desee.',
 
     expectedArgs: '<id>',
     minArgs: 1,
@@ -15,15 +15,15 @@ module.exports = {
         const reply = { custom: true };
         var deleted = 0;
         const user = await client.users.fetch(targetId).catch(console.error);
-        await user.createDM();
-        const messages = await user.dmChannel.messages.fetch().catch(console.error);
-        messages.forEach(async m => {
+        const dmChannel = await user.createDM().catch(console.error);
+        const messages = await dmChannel.messages.fetch().catch(console.error);
+        for (const m of messages) {
             if (m.author.id === ids.users.bot) {
-                await m.delete();
+                await m.delete().catch(console.error);
                 deleted++;
                 await new Promise(res => setTimeout(res, 1000 * 5));
             }
-        });
+        }
         reply.content = deleted > 0 ? `Se borraron **${deleted} mensajes**.` : 'Este usuario no tiene ningún mensaje directo.';
         return reply;
     }
