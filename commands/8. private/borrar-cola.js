af5343293e11ef14d0b691206a0aa3f87b3eb9fe
Nicolás Céspede <cespede.alannicolas.42984@gmail.com>
2022-08-11 22:57:03 -0300

    Agregado -borrar-cola

---
+++ commands/8. private/borrar-cola.js
@@ -0,0 +1,15 @@
+module.exports = {
+    category: 'Privados',
+    description: 'Elimina la cola de reproducción previa de la base de datos.',
+
+    maxArgs: 0,
+    slash: 'both',
+    ownerOnly: true,
+
+    callback: async () => {
+        const reply = { custom: true };
+        await executeQuery('DELETE FROM "previousQueue";').catch(console.error);
+        reply.content = `> Cola de reproducción previa eliminada de la base de datos`;
+        return reply;
+    }
+}
