4b689a90dfe1bd99577efc9e7eae089ea7c3e828
Nicolás Céspede <cespede.alannicolas.42984@gmail.com>
2022-09-13 05:20:30 -0300

    Intento de fix 2

--- commands/7. private/boton-rol.js
+++ commands/7. private/boton-rol.js
@@ -4,25 +4,6 @@ const { getRolesMessageInfo, updateRolesMessageInfo } = require("../../app/cache
 const buttonStyles = ['primary', 'secondary', 'success', 'danger'];
 const prefix = 'button-roles-';
 
-const getButtonStyle = string => {
-    let buttonStyle;
-    switch (string) {
-        case 'primary':
-            buttonStyle = ButtonStyle.Primary;
-            break;
-        case 'secondary':
-            buttonStyle = ButtonStyle.Secondary;
-            break;
-        case 'success':
-            buttonStyle = ButtonStyle.Success;
-            break;
-        case 'danger':
-            buttonStyle = ButtonStyle.Danger;
-            break;
-    }
-    return buttonStyle;
-};
-
 module.exports = {
     category: 'Privados',
     description: 'Agrega un botón al mensaje de roles.',
@@ -52,7 +33,7 @@ module.exports = {
             description: 'El estilo para el botón.',
             required: true,
             type: ApplicationCommandOptionType.String,
-            choices: buttonStyles.map(style => ({ name: style, value: getButtonStyle(style) }))
+            choices: buttonStyles.map(style => ({ name: style, value: style.toLowerCase() }))
         },
         {
             name: 'etiqueta',
@@ -101,7 +82,20 @@ module.exports = {
         if (!buttonStyles.includes(buttonStyle.toLowerCase()))
             return `Estilo de botón desconocido. Los estilos válidos son: _"${buttonStyles.join('", "')}"_.`;
 
-        buttonStyle = getButtonStyle(buttonStyle);
+        switch (buttonStyle) {
+            case 'primary':
+                buttonStyle = ButtonStyle.Primary;
+                break;
+            case 'secondary':
+                buttonStyle = ButtonStyle.Secondary;
+                break;
+            case 'success':
+                buttonStyle = ButtonStyle.Success;
+                break;
+            case 'danger':
+                buttonStyle = ButtonStyle.Danger;
+                break;
+        }
 
         const buttonLabel = args.shift();
 
