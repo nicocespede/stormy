cfb184ece2f1036633635dcd9a3764bf65862808
Nicolás Céspede <cespede.alannicolas.42984@gmail.com>
2022-09-23 02:25:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const path = require('path');
 require('dotenv').config();
 const { Player } = require('discord-player');
 const chalk = require('chalk');
-chalk.enabled = true;
-chalk.level = 3;
+chalk.level = 1;
 const cache = require('./app/cache');
 const { convertTZ, initiateReactionCollector, periodicFunction, pushDifference, checkBansCorrelativity, startStatsCounters, countMembers,
     countConnectedMembers, checkKruUpcomingMatches } = require('./app/general');
