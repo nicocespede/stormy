e06dadbb9fc53050478215c762f83f17b478c24d
Nicolás Céspede <cespede.alannicolas.42984@gmail.com>
2023-09-17 18:47:15 -0300

    Agregados embeds en -desbanear

--- features/unban-message.js
+++ features/unban-message.js
@@ -1,25 +1,40 @@
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, Client } = require("discord.js");
 const { getBanned, updateBanned, getIds } = require("../src/cache");
-const { getUserTag } = require("../src/util");
+const { getUserTag, consoleLogError, logToFileError } = require("../src/util");
 const { getUnbannedMemberEmbedInfo } = require("../src/characters");
 const { deleteBan } = require("../src/mongodb");
 
+const MODULE_NAME = 'features.unban-message';
+
+/** @param {Client} client */
 module.exports = client => {
     client.on('guildBanRemove', async ban => {
         const banned = getBanned() || await updateBanned();
-        if (Object.keys(banned).includes(ban.user.id)) {
-            await deleteBan(ban.user.id).catch(console.error);
-            await updateBanned();
+        const { user } = ban;
+
+        if (Object.keys(banned).includes(user.id)) {
+            try {
+                await deleteBan(user.id);
+                await updateBanned();
+            } catch (error) {
+                consoleLogError('> Error al eliminar ban de la base de datos');
+                logToFileError(MODULE_NAME, error);
+            }
+
             const ids = await getIds();
-            const channel = await client.channels.fetch(ids.channels.welcome).catch(console.error);
-            const embedInfo = await getUnbannedMemberEmbedInfo(getUserTag(ban.user), banned[ban.user.id].character);
-            channel.send({
-                embeds: [new EmbedBuilder()
-                    .setTitle(embedInfo.title)
-                    .setDescription(embedInfo.description)
-                    .setColor(embedInfo.color)
-                    .setThumbnail(embedInfo.thumbnail)]
-            });
+            const channel = await client.channels.fetch(ids.channels.welcome);
+            if (!channel)
+                consoleLogError('> Error al obtener canal de bienvenida');
+            else {
+                const embedInfo = await getUnbannedMemberEmbedInfo(getUserTag(user), banned[user.id].character);
+                channel.send({
+                    embeds: [new EmbedBuilder()
+                        .setTitle(embedInfo.title)
+                        .setDescription(embedInfo.description)
+                        .setColor(embedInfo.color)
+                        .setThumbnail(embedInfo.thumbnail)]
+                });
+            }
         }
     });
 };
