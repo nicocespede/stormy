c7fd9f9dd67f382e1cc0ae8ebc4036d42ec71d52
Nicolás Céspede <cespede.alannicolas.42984@gmail.com>
2023-09-28 19:04:08 -0300

    Agregada nueva thumb de sombra

--- commands/4. moderation/sombra.js
+++ commands/4. moderation/sombra.js
@@ -1,6 +1,11 @@
+const { ICommand } = require("wokcommands");
 const { EmbedBuilder } = require('discord.js');
 const { getSombraBans, updateSombraBans, getGithubRawUrl } = require('../../src/cache');
+const { logToFileCommandUsage } = require('../../src/util');
 
+const thumbs = ['sombra', 'sombra-lgbt'];
+
+/**@type {ICommand}*/
 module.exports = {
     category: 'Moderación',
     description: 'Responde con el historial de baneos de Sombra.',
@@ -8,7 +13,9 @@ module.exports = {
     maxArgs: 0,
     slash: 'both',
 
-    callback: async ({ instance }) => {
+    callback: async ({ instance, interaction, text, user }) => {
+        logToFileCommandUsage('sombra', text, interaction, user);
+
         const bans = getSombraBans() || await updateSombraBans();
         let description = `**Sombra#9370** fue baneado ${bans.length} veces. A continuación la lista de las razones:\n\n`;
         for (var i = 0; i < bans.length; i++) {
@@ -16,13 +23,15 @@ module.exports = {
             description += `**${i + 1}:** ${!reason || reason === '' ? 'Sin razón de baneo.' : reason}\n`;
         }
         description = description.replace(/[\\\n]+n/g, '\n');
+
+        const random = Math.floor(Math.random() * (thumbs.length));
         return {
             custom: true,
             embeds: [new EmbedBuilder()
                 .setTitle('Bans de **Sombra#9370**')
                 .setDescription(description)
                 .setColor(instance.color)
-                .setThumbnail(await getGithubRawUrl('assets/thumbs/sombra.jpeg'))],
+                .setThumbnail(await getGithubRawUrl(`assets/thumbs/${thumbs[random]}.jpeg`))],
             ephemeral: true
         };
     }
